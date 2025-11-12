a1fcf43356ee7f3bf63f4bcb4293d865b6a18781
Tejas Kalyanshetty <tejaskalyanshetty@MacBookPro.fritz.box>
2025-11-12 01:51:14 +0100

    feat(F1,F2,F3): complete v2.0 discovery UI and 5-pack flow~

---
+++ src/constants/pantry.js
@@ -0,0 +1,32 @@
+// src/constants/pantry.js
+// This file contains the master list of pantry staples for v2.0
+// This list is based on research (Story 1.3) and is used to filter the shopping list.
+export const PANTRY_STAPLES = [
+    // Oils & Vinegars
+    "olive oil", "vegetable oil", "sesame oil", "coconut oil", "canola oil",
+    "balsamic vinegar", "red wine vinegar", "white vinegar", "apple cider vinegar", "rice vinegar",
+    
+    // Spices (Basic)
+    "salt", "black pepper", "pepper", "chilli flakes", "red chilli flakes",
+    "paprika", "smoked paprika", "cayenne pepper", "chilli powder", "cumin",
+    "coriander", "garlic powder", "onion powder", "oregano", "thyme", "rosemary", "bay leaves",
+  
+    // Spices (Global)
+    "garam masala", "turmeric", "curry powder", "cinnamon", "nutmeg", "cloves",
+    "cardamom", "star anise", "five-spice powder", "za'atar", "sumac",
+    
+    // Condiments & Sauces
+    "soy sauce", "tamari", "fish sauce", "oyster sauce", "hoisin sauce", "sriracha",
+    "hot sauce", "worcestershire sauce", "ketchup", "mayonnaise", "dijon mustard",
+    "yellow mustard", "honey", "maple syrup",
+  
+    // Baking & Grains
+    "all-purpose flour", "plain flour", "sugar", "brown sugar", "powdered sugar",
+    "baking soda", "baking powder", "cornstarch", "cocoa powder", "vanilla extract",
+    "breadcrumbs", "panko breadcrumbs", "rice", "basmati rice", "jasmine rice", "pasta",
+    "quinoa", "oats", "lentils", "chickpeas", "black beans", "kidney beans",
+    "canned tomatoes", "chopped tomatoes", "tomato paste", "coconut milk",
+  
+    // Core Aromatics (often filtered)
+    "garlic", "onion", "onions", "ginger", "water"
+  ];
